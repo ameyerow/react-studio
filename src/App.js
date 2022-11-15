69b59dfcf76d1d56baf4d573b1ee7c673a25db63
Alex Meyerowitz <alexandermeyerowitz@yahoo.com>
2022-11-15 17:00:05 -0500

    Studio

--- src/App.js
+++ src/App.js
@@ -8,21 +8,58 @@ bakeryData.forEach((item) => {
 });
 /* ############################################################## */
 
+function BakeryItem(item, addBakeryItemToCart) {
+  return ( 
+    <div class="BakeryItem">
+      <img src={item.image} alt={item.name}></img>
+      <h1>{item.name}</h1>
+      <p>{item.description}</p>
+      <div>
+        <p>{item.price}</p>
+        <button onClick={() => addBakeryItemToCart(item.name, item.price)}>Add to Cart</button>
+      </div>
+    </div>
+  );
+}
+
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [totalCheckoutPrice, setTotalCheckoutPrice] = useState(0);
+  const [checkoutItems, setCheckoutItems] = useState(new Map());
+
+  const addBakeryItemToCart = (name, price) => {  
+    let items = new Map(checkoutItems);
+    if (items.has(name)) {
+      items.set(name, items.get(name) + 1);
+    } else {
+      items.set(name, 1);
+    }
+    setCheckoutItems(items);
+    setTotalCheckoutPrice(totalCheckoutPrice + price);
+  }
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <div class="bakery">
+        <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
+        <div class="gallery-container">
+          {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+            BakeryItem(item, addBakeryItemToCart) // replace with BakeryItem component
+          ))}
+        </div>
+      </div>
+      
       <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        <div className="cart">
+          <h2>Cart</h2>
+          {/* TODO: render a list of items in the cart */}
+          {[...checkoutItems].map(([name, numItems]) => (
+            <p>{name} x {numItems}</p>
+          ))}
+          <p>Total: ${totalCheckoutPrice.toFixed(2)}</p>
+        </div>
       </div>
     </div>
   );
